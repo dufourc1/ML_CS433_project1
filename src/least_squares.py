997b996d0b238ebd593770dce393e012313d5f92
Charles Dufour <charles.dufour@epfl.ch>
2018-10-07 11:42:04 +0200

    gradient descent

--- src/least_squares.py
+++ src/least_squares.py
@@ -8,9 +8,22 @@ from costs import *
 from gradient_descent import *
 from stochastic_gradient_descent import *
 
+def compute_gradient(y,tx,w):
+    """Compute the gradient."""
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
-    
+    # Define parameters to store w and loss
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute loss, gradient
+        grad, err = compute_gradient(y, tx, w)
+        loss = calculate_mse(err)
+        # gradient w by descent update
+        w = w - gamma * grad
+        #print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
     return (w,loss)
 
 def least_squares_SGD(y, tx, initial_w,max_iters, gamma):
